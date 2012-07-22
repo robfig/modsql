2f52da8d217a49f772ccc814fc4c37dbbc3e9f74
Jonas mg <jonas@matx.cc>
2012-07-22 10:09:06 +0100

    Fix typo in doc.

--- doc.go
+++ doc.go
@@ -8,19 +8,17 @@
 Package modsql allows use a Go model to define the database model and generate
 its corresponding SQL language and Go types. It is not an ORM neither it is not
 going to be it since an ORM creates an extra layer to the database access.
+The API is based in SQLAlchemy's (http://www.sqlalchemy.org/).
 
-It generates the files "model.sql" and "model.go" at writing to the file system.
-But it also can shows the generated output.
-
-The API is based in SQLAlchemy's (http://www.sqlalchemy.org/), althought it has
-been only added some basic types.
+It generates the files "model.sql" and "model.go" at writing to the file system;
+it also can shows the generated output.
 
 The function NewBuffer has the method Mode to create tables related to
 localization. If it is set, then at generating SQL it is created an extra table
 (starting wich "_") for each model.
 
-NOTE: it is tested with SQLite3 and PostgreSQL. And it is not ready to working
-with relations between tables since I don't need it by now.
+NOTE: it is not ready to working with relations between tables since I don't
+need it by now.
 
 
 Operating instructions
@@ -36,19 +34,21 @@ To run it, use "go run file.go".
 		metadata := NewMetadata(PostgreSQL).Mode(Help)
 
 		types := Table("types", metadata,
-			Column("id", Integer).PrimaryKey(),
-			Column("t_int", Integer),
-			Column("t_float", Float),
-			Column("t_text", Text),
-			Column("t_bool", Boolean),
+			Column("id", Int64).PrimaryKey(),
+			Column("t_int", Int32),
+			Column("t_float", Float64),
+			Column("t_string", String),
+			//Column("t_binary", Binary),
+			Column("t_bool", Bool),
 		)
 
 		def := Table("default_value", metadata,
-			Column("id", Integer).PrimaryKey(),
-			Column("d_int", Integer).Default(55),
-			Column("d_float", Float).Default(10.2),
-			Column("d_text", Text).Default("string"),
-			Column("d_bool", Boolean).Default(false),
+			Column("id", Int64).PrimaryKey(),
+			Column("d_int", Int8).Default(int8(55)),
+			Column("d_float", Float32).Default(float32(10.2)),
+			Column("d_string", String).Default("string"),
+			//Column("d_binary", Binary).Default([]byte("123")),
+			Column("d_bool", Bool).Default(false),
 		)
 
 		// == Insert values
