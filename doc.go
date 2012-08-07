c4183cdfbd1f5b046422b2a7a2ec933ca8c11112
Jonas mg <jonas@matx.cc>
2012-08-07 20:01:23 +0100

    Fix doc.

--- doc.go
+++ doc.go
@@ -10,16 +10,17 @@ its corresponding SQL language and Go types. It is not an ORM neither it is not
 going to be it since an ORM creates an extra layer to the database access.
 The API is based in SQLAlchemy's (http://www.sqlalchemy.org/).
 
-It generates the files "model.sql" and "model.go" at writing to the file system;
-it also can shows the generated output.
+It generates the files SQL and Go at writing to the file system, but it also can
+shows the generated output. The name for the generated files is got using the
+prefix "data-" plus the directory name where is the ModSQL file.
 
 If it is used the type Int, then the SQL files will have variables delimited by
 "{{" and "}}", which will be parsed by the function Load according to the
 architecture where it is being run.
 
-The function NewBuffer has the method Mode to create tables related to
-localization. If it is set, then at generating SQL it is created an extra table
-(starting wich "_") for each model.
+The function Metadata has the mode Help to create tables related to localization.
+If it is set, then at generating SQL it is created an extra table (starting wich
+"_") for each model.
 
 NOTE: it is not ready to working with relations between tables since I don't
 need it by now.
